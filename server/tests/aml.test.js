e8541374a1812a823c5fceb9b1553d2393f08ee9
RAKESH BEHERA <202935642+11RAKESH11@users.noreply.github.com>
2026-09-15 15:29:44 +0530

    Update aml.test.js

--- server/tests/aml.test.js
+++ server/tests/aml.test.js
@@ -1,15 +1,4 @@
 const { calculateRiskScore, classifyRisk } = require("../services/aml/riskScoring");
-
-
-
-
-
-
-
-
-
-
-
 describe("AML risk scoring", () => {
   test("caps score at 100", () => {
     expect(calculateRiskScore([70, 60])).toBe(100);
