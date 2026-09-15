847817e5f5b204b9df6e82a9dd9ddc97eace325b
RAKESH BEHERA <202935642+11RAKESH11@users.noreply.github.com>
2026-09-15 15:30:14 +0530

    Clean up auth.test.js by removing blank lines
    Removed unnecessary blank lines in the auth test file.

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,10 +1,5 @@
 const { calculateRiskScore, classifyRisk } = require("../services/aml/riskScoring");
 
-
-
-
-
-
 describe("Basic authentication-related utility setup", () => {
   test("risk utility can be imported", () => {
     expect(calculateRiskScore([20, 30])).toBe(50);
