2f031f8155179dfe1434b922e39daa9f8e39ac8a
RAKESH BEHERA <202935642+11RAKESH11@users.noreply.github.com>
2026-09-15 15:30:00 +0530

    Add tests for calculateRiskScore and classifyRisk
    Added tests for risk scoring utilities.

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,5 +1,10 @@
 const { calculateRiskScore, classifyRisk } = require("../services/aml/riskScoring");
 
+
+
+
+
+
 describe("Basic authentication-related utility setup", () => {
   test("risk utility can be imported", () => {
     expect(calculateRiskScore([20, 30])).toBe(50);
