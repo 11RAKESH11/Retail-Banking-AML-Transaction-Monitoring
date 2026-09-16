41391af281c510f2c261c369bbc022a951e1e2f5
RAKESH BEHERA <202935642+11RAKESH11@users.noreply.github.com>
2026-09-16 18:51:34 +0530

    Remove unnecessary empty line in auth test

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,5 +1,4 @@
 const { calculateRiskScore, classifyRisk } = require("../services/aml/riskScoring");
-
 describe("Basic authentication-related utility setup", () => {
   test("risk utility can be imported", () => {
     expect(calculateRiskScore([20, 30])).toBe(50);
